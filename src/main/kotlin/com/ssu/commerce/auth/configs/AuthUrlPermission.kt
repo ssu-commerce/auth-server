c4d74c24e21da0cd68ef4093c84bbd6fbd83345f
always0ne <always0ne.dev@gmail.com>
2022-06-19 22:17:34 +0900

    chore: 코어 버전업

--- src/main/kotlin/com/ssu/commerce/auth/configs/AuthUrlPermission.kt
+++ src/main/kotlin/com/ssu/commerce/auth/configs/AuthUrlPermission.kt
@@ -2,6 +2,7 @@ package com.ssu.commerce.auth.configs
 
 import com.ssu.commerce.core.configs.UrlPermissionFilter
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer
@@ -9,6 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 
+@Primary
 @Component
 class AuthUrlPermission : UrlPermissionFilter {
     override fun urlPermissions(authorizeRequests: ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry): ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry =

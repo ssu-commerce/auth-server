a190db1845e7f4b97856340ef347d401659da12a
always0ne <always0ne.dev@gmail.com>
2024-03-27 23:49:55 +0900

    api path 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,42 +5,53 @@ plugins {
     id("org.springframework.boot") version "2.6.6"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("com.ssu.commerce.plugin.github-registry")
-    id("com.ssu.commerce.plugin.docker-publish")
-    id("com.ssu.commerce.plugin.maven-publish")
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
     kotlin("plugin.jpa") version "1.6.10"
 }
-group = "com.ssu.commerce"
-version = System.getenv("VERSION") ?: "NoVersion"
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-}
-
-configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
+tasks {
+    jar {
+        enabled = false
+    }
+    bootJar {
+        enabled = false
     }
 }
 
-dependencies {
-    implementation("com.ssu.commerce:ssu-commerce-core-web:${properties["ssu-commerce-core"]}")
-    implementation("com.ssu.commerce:ssu-commerce-core-security:${properties["ssu-commerce-core"]}")
-    implementation("com.ssu.commerce:ssu-commerce-core-jpa:${properties["ssu-commerce-core"]}")
+subprojects {
+    apply(plugin = "kotlin")
+    apply(plugin = "kotlin-spring")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "com.ssu.commerce.plugin.github-registry")
 
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.security:spring-security-test")
-}
+    group = "com.ssu.commerce"
+    version = System.getenv("VERSION") ?: "NoVersion"
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+    java {
+        sourceCompatibility = JavaVersion.VERSION_11
     }
-}
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+    configurations {
+        compileOnly {
+            extendsFrom(configurations.annotationProcessor.get())
+        }
+    }
+
+    dependencies {
+        annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "11"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 }

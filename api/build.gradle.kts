4aed5df39dabe810bfe9731d482aef2917f32589
always0ne <always0ne.dev@gmail.com>
2024-03-27 23:50:44 +0900

    api path 수정

---
+++ api/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("com.ssu.commerce.plugin.docker-publish")
+}
+
+dependencies {
+    implementation("com.ssu.commerce:ssu-commerce-core-web:${properties["ssu-commerce-core"]}")
+    implementation("com.ssu.commerce:ssu-commerce-core-security:${properties["ssu-commerce-core"]}")
+    implementation("com.ssu.commerce:ssu-commerce-core-jpa:${properties["ssu-commerce-core"]}")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+}

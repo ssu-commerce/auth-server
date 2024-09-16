9b9cbf5db1958af3f582d35bfcb9646ab2cab035
always0ne <always0ne.dev@gmail.com>
2024-09-16 20:38:37 +0900

    core version up

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -6,6 +6,7 @@ dependencies {
     implementation("com.ssu.commerce:ssu-commerce-core-web:${properties["ssu-commerce-core"]}")
     implementation("com.ssu.commerce:ssu-commerce-core-security:${properties["ssu-commerce-core"]}")
     implementation("com.ssu.commerce:ssu-commerce-core-jpa:${properties["ssu-commerce-core"]}")
+    implementation("com.ssu.commerce:ssu-commerce-core-monitoring:${properties["ssu-commerce-core"]}")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
